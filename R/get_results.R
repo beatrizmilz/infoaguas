5d53bf832ac32be84cb4d321d69515ee5d52afea
Beatriz Milz <beatriz.milz@hotmail.com>
2020-11-26 17:50:15 -0300

    funcao get_results, avanço mas ainda não funciona

--- R/get_results.R
+++ R/get_results.R
@@ -1,15 +1,29 @@
-
-
-
-
-
 get_results <- function(sampling_point, start, end) {
-
   `%>%` <- magrittr::`%>%`
 
-  sampling_point <- 145
+
+  n_ugrhi <- 6
+  sampling_point <- 106
   start <- "01/01/2020"
   end <- "25/11/2020"
+
+
+  u_busca <-
+    "https://sistemainfoaguas.cetesb.sp.gov.br/AguasSuperficiais/RelatorioQualidadeAguasSuperficiais"
+
+
+  body_busca <- list(
+    "TipoConsulta" = "Monitoramento",
+    "FiltroTipo" = "1",
+    "NUGRHI" = n_ugrhi,
+    "X-Requested-With" = "XMLHttpRequest"
+
+  )
+
+
+  r_busca <- httr::POST(u_busca, body = body_busca, encode = "form")
+
+
   u_busca <-
     "https://sistemainfoaguas.cetesb.sp.gov.br/AguasSuperficiais/RelatorioQualidadeAguasSuperficiais/MonitoramentoModal"
 
@@ -17,17 +31,24 @@ get_results <- function(sampling_point, start, end) {
 
 
   body_busca <- list(
-
-      "DataInicial" = start,
-      "DataFinal" = end ,
-      "CodigoPonto[]" = sampling_point
+    "DataInicial" = start,
+    "DataFinal" = end ,
+    "CodigoPonto[]" = sampling_point
 
   )
 
 
-  r_busca <- httr::POST(u_busca, body = body_busca, encode = "form")
+  r_busca <- httr::POST(
+    u_busca,
+ #   query = list(method = "pesquisar"),
+    body = body_busca,
+    encode = "form"#,
+  #  httr::accept("xlsx")
+  )
 
   httr::content(r_busca)
+
+
 
 
   r_monitoramento <-
@@ -37,8 +58,6 @@ get_results <- function(sampling_point, start, end) {
     )
 
 
-  # # erro:
-  # Valor não pode ser nulo.
-  # Nome do parâmetro: source
+  # Até aqui: o html pede para fazer a busca pela data (mas já fiz....) (?)
 
 }
--- R/get_sampling_points.R
+++ R/get_sampling_points.R
@@ -30,7 +30,8 @@ get_sampling_points <- function(n_ugrhi) {
 
   r_monitoramento <-
     httr::GET(
-      "https://sistemainfoaguas.cetesb.sp.gov.br/AguasSuperficiais/RelatorioQualidadeAguasSuperficiais/Monitoramento")
+      "https://sistemainfoaguas.cetesb.sp.gov.br/AguasSuperficiais/RelatorioQualidadeAguasSuperficiais/Monitoramento"
+    )
 
 
   id_ponto <- r_monitoramento %>%
@@ -49,21 +50,27 @@ get_sampling_points <- function(n_ugrhi) {
     tibble::as_tibble() %>%
     tibble::add_column(id_ponto) %>%
     dplyr::select(-x) %>%
-    dplyr::mutate(data_inicio = readr::parse_date(data_inicio, format = "%d/%m/%Y"),
-                  data_fim = as.character(data_fim),
-                  data_fim = readr::parse_date(data_fim, format = "%d/%m/%Y"),
-                  n_ugrhi = n_ugrhi,
-                  muni = abjutils::rm_accent(municipio),
-                  muni = stringr::str_replace_all(muni, "-", " "),
-                  muni = dplyr::case_when(muni == "SANTANA DO PARNAIBA" ~
-                                            "SANTANA DE PARNAIBA",
-                                          muni == "QUEIROS" ~ "QUEIROZ",
-                                          TRUE ~ muni))
+    dplyr::mutate(
+      data_inicio = readr::parse_date(data_inicio, format = "%d/%m/%Y"),
+      data_fim = as.character(data_fim),
+      data_fim = readr::parse_date(data_fim, format = "%d/%m/%Y"),
+      n_ugrhi = n_ugrhi,
+      muni = abjutils::rm_accent(municipio),
+      muni = stringr::str_replace_all(muni, "-", " "),
+      muni = dplyr::case_when(
+        muni == "SANTANA DO PARNAIBA" ~
+          "SANTANA DE PARNAIBA",
+        muni == "QUEIROS" ~ "QUEIROZ",
+        TRUE ~ muni
+      )
+    )
 
- join_code_muni <- tabela_pontos %>% dplyr::left_join(municipios_sp, by = "muni") %>% dplyr::select(-muni)
+  join_code_muni <-  tabela_pontos %>%
+    dplyr::left_join(municipios_sp, by = "muni") %>%
+    dplyr::select(-muni)
 
 
- join_code_muni
+  join_code_muni
 
 
 }
