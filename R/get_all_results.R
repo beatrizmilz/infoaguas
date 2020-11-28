239cc9f29cc5f86c0d6088ebc519e29343542086
Beatriz Milz <beatriz.milz@hotmail.com>
2020-11-28 15:18:20 -0300

    altera funcoes get results

--- R/get_all_results.R
+++ R/get_all_results.R
@@ -1,41 +1,44 @@
 get_all_results <- function(vetor_pontos, path_download) {
-  future::plan(future::multisession)
+  # future::plan(future::multisession)
 
-  #progressr::with_progress({
-    hoje <- format(Sys.Date(), "%d-%m-%Y")
-
-
-    inicial <- paste0("01-01-", c(seq(
-      from = 1974, to = 2020, by = 5
-    )))
+  hoje <- format(Sys.Date(), "%d-%m-%Y")
 
 
-    final <-
-      c(paste0("31-12-", c(seq(
-        from = 1979, to = 2020, by = 5
-      ) - 1)),
-      hoje)
+  inicial <- paste0("01-01-", c(seq(
+    from = 1974, to = 2020, by = 5
+  )))
 
 
-    for (i in 1:length(inicial)) {
-      inicial_busca <- inicial[i]
-      final_busca <- final[i]
-
-      furrr::future_map(
-        .x = vetor_pontos,
-        .f = get_results,
-        start = inicial_busca,
-        end = final_busca,
-        path = path_download
-      )
-
-      Sys.sleep(1)
-
-    }
-   #p()
+  final <-
+    c(paste0("31-12-", c(seq(
+      from = 1979, to = 2020, by = 5
+    ) - 1)),
+    hoje)
 
 
-  #})
+  for (i in 1:length(inicial)) {
+
+    inicial_busca <- inicial[i]
+    final_busca <- final[i]
+
+    # furrr::future_map(
+    #   .x = vetor_pontos,
+    #   .f = get_results,
+    #   start = inicial_busca,
+    #   end = final_busca,
+    #   path = path_download
+    # )
+
+    purrr::map(
+      .x = vetor_pontos,
+      .f = get_results,
+      start = inicial_busca,
+      end = final_busca,
+      path = path_download
+    )
+  }
+
+
 
 
 }
--- R/get_results.R
+++ R/get_results.R
@@ -1,67 +1,75 @@
 get_results <- function(sampling_point, start, end, path) {
   `%>%` <- magrittr::`%>%`
 
-  u_busca <-
-    "https://sistemainfoaguas.cetesb.sp.gov.br/AguasSuperficiais/RelatorioQualidadeAguasSuperficiais"
+  data_inicio_ponto <-
+    sampling_points %>%
+    dplyr::filter(cod_interaguas == sampling_point) %>%
+    dplyr::pull(data_inicio)
 
+  continuar <-
+    lubridate::as_date(data_inicio_ponto) < lubridate::as_date(start, format = "%d-%m-%Y")
 
-  res <- httr::GET(u_busca)
-
-
-  body_busca <- list(
-    "TipoConsulta" = "Monitoramento",
-    "FiltroTipo" = "0",
-    "X-Requested-With" = "XMLHttpRequest"
-
-  )
-
-
-  r_busca <-
-    httr::POST(u_busca, body = body_busca, encode = "form")
-
-
-  u_busca <-
-    "https://sistemainfoaguas.cetesb.sp.gov.br/AguasSuperficiais/RelatorioQualidadeAguasSuperficiais/MonitoramentoModal"
-
-
-
-
-  body_busca <- list(
-    "DataInicial" = start,
-    "DataFinal" = end ,
-    "CodigoPonto[]" = sampling_point
-
-  )
-
-
-  r_busca <- httr::POST(u_busca,
-                        body = body_busca,
-                        encode = "form")
-
-  resposta <- httr::content(r_busca) %>% xml2::xml_text()
-
-  if (resposta == "success") {
-    start_2 <- stringr::str_replace_all(start, "/", "-")
-
-    end_2 <- stringr::str_replace_all(end, "/", "-")
-
-    arquivo <-
-      glue::glue("{path}infoaguas_{sampling_point}_start-{start_2}_end-{end_2}.xlsx")
-
-    r_monitoramento <-
-      httr::GET(
-        "https://sistemainfoaguas.cetesb.sp.gov.br/AguasSuperficiais/RelatorioQualidadeAguasSuperficiais/Download",
-        httr::write_disk(arquivo, overwrite = TRUE)
-      )
-    message(glue::glue("O arquivo foi baixado e pode ser encontrado em:
-                     {arquivo}"))
-  } else {
+  if (continuar == FALSE) {
     message(
       glue::glue(
-        "Arquivo não baixado, referente ao ponto {sampling_point} nos períodos {start} a {end}."
+        "Arquivo não baixado, referente ao ponto {sampling_point} nos períodos {inicial_busca} a {final_busca}.
+              Não há dados disponíveis para este período.
+              "
       )
     )
+  } else {
+    u_busca <-
+      "https://sistemainfoaguas.cetesb.sp.gov.br/AguasSuperficiais/RelatorioQualidadeAguasSuperficiais/MonitoramentoModal"
+
+    body_busca <- list(
+      "DataInicial" = start,
+      "DataFinal" = end ,
+      "CodigoPonto[]" = sampling_point
+
+    )
+
+
+    r_busca <- httr::POST(u_busca,
+                          body = body_busca,
+                          encode = "form")
+
+    resposta <- httr::content(r_busca) %>% xml2::xml_text()
+
+    if (resposta == "success") {
+      start_2 <- stringr::str_replace_all(start, "/", "-")
+
+      end_2 <- stringr::str_replace_all(end, "/", "-")
+
+      arquivo <-
+        glue::glue("{path}infoaguas_{sampling_point}_start-{start_2}_end-{end_2}.xlsx")
+
+      r_monitoramento <-
+        httr::GET(
+          "https://sistemainfoaguas.cetesb.sp.gov.br/AguasSuperficiais/RelatorioQualidadeAguasSuperficiais/Download",
+          httr::write_disk(arquivo, overwrite = TRUE)
+        )
+      message(glue::glue("O arquivo foi baixado e pode ser encontrado em:
+                     {arquivo}"))
+    } else {
+      message(
+        glue::glue(
+          "Arquivo não baixado, referente ao ponto {sampling_point} nos períodos {start} a {end}."
+        )
+      )
+    }
+
   }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
