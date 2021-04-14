dfba00bd1c879491b187eed9b4dde678218fd81b
Beatriz Milz <beatriz.milz@hotmail.com>
2021-04-14 11:32:07 -0300

    mais uma sugestão do @jtrecenti

--- data-raw/get_all.R
+++ data-raw/get_all.R
@@ -1,3 +1,31 @@
+maybe_get_results_progresso <- function(points, path, prog) {
+  if (!missing(prog))
+    prog()
+
+  maybe_get_results <-
+    purrr::possibly(get_results, otherwise = "Erro")
+
+
+  maybe_get_results(points, path)
+}
+
+get_all_results <-  function(points, path) {
+  # coloca o script no contexto
+  progressr::with_progress({
+    # cria a barra de progresso
+    p <- progressr::progressor(length(points))
+
+    purrr::walk(points,
+                maybe_get_results_progresso,
+                path = path,
+                prog = p)
+
+
+  })
+}
+
+
+
 # Primeira rodada de download dos arquivos!
 todos_codigos <- unique(sampling_points$cod_interaguas)
 
